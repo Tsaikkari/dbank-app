c84a606a440a065a7d17b01b0473e25bc1d4a873
Tsaikkari <kirsi.trospe@gmail.com>
2024-04-03 23:02:39 +0200

    Initial commit

--- src/dbankapp_backend/main.mo
+++ src/dbankapp_backend/main.mo
@@ -1,5 +1,34 @@
-actor {
-  public query func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+import Debug "mo:base/Debug";
+import Time "mo:base/Time";
+import Float "mo:base/Float";
+
+actor DBankApp {
+  stable var currentValue: Float = 400;
+  stable var startTime = Time.now();
+  
+  public func topUp(amount: Float) {
+    currentValue += amount;
+  };
+
+  public func withdraw(amount: Float) {
+    let temp: Float = currentValue - amount;
+    if (temp >= 0) {
+      currentValue -= amount;
+    } else {
+      Debug.print("Amount too large");
+    }
+  };
+
+  public query func checkBalance(): async Float {
+    return currentValue;
+  };
+
+  public func compound() {
+    let currentTime = Time.now();
+    let timeElapsedSeconds = (currentTime - startTime) / 1000000000;
+    currentValue := currentValue * (1.00000000001 ** Float.fromInt(timeElapsedSeconds));
+    // reset startTime for each compound
+    startTime := currentTime;
+    Debug.print(debug_show(currentValue));
   };
 };
--- src/dbankapp_frontend/src/App.jsx
+++ src/dbankapp_frontend/src/App.jsx
@@ -1,30 +1,68 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { dbankapp_backend } from 'declarations/dbankapp_backend';
 
 function App() {
-  const [greeting, setGreeting] = useState('');
+  const [currentValue, setCurrentValue] = useState(null);
+  const [isLoading, setIsLoading] = useState(true)
+  const [formData, setFormData] = useState({
+    topUpAmount: '',
+    withdrawalAmount: '',
+  });
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    dbankapp_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
+  const currentBalance = () => {
+    return dbankapp_backend.compound()
+      .then(() => {
+        dbankapp_backend.checkBalance()
+      .then((res) => {
+        setCurrentValue(res);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setIsLoading(false)
+        console.log(err)
+      })
+    })
+  };
+
+  useEffect(() => {
+    currentBalance();
+  }, []);
+
+  function handleChange(e) {
+    const {value, name} = e.target;
+    setFormData(prevValue => {
+      return {
+        ...prevValue,
+        [name]: value,
+      }
     });
-    return false;
+  }
+
+  function handleClick(e) {
+    e.preventDefault();
+    dbankapp_backend.topUp(Number(formData.topUpAmount));
+    dbankapp_backend.withdraw(Number(formData.withdrawalAmount));
+    currentBalance(); 
+    setFormData({
+      topUpAmount: '',
+      withdrawalAmount: '',
+    });
   }
 
   return (
-    <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
+    <div className="container">
+      {isLoading && <h5>Loading ...</h5>}
+      {currentValue &&
+      <h1>Current Balance: <span id="value"></span>{currentValue.toFixed(2)}</h1>}
+      <div className="divider"></div>
+      <form typeof="submit">
+        <h2>Amount to Top Up</h2>
+        <input className="input-amount" type="number" min="0" name="topUpAmount" value={formData.topUpAmount} onChange={handleChange} />
+        <h2>Amount to Withdraw</h2>
+        <input className="withdrawal-amount" type="number" min="0" name="withdrawalAmount" value={formData.withdrawalAmount} onChange={handleChange} />
+        <input className="submit-btn" onClick={handleClick} placeholder="Finalise Transaction" />
       </form>
-      <section id="greeting">{greeting}</section>
-    </main>
+    </div>
   );
 }
 
