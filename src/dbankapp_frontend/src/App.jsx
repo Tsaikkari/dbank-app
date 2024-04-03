c4eeb181c48b35b8a2ae24680477ba86a3052956
Tsaikkari <kirsi.trospe@gmail.com>
2024-04-03 15:33:59 +0200

    Initial commit.

---
+++ src/dbankapp_frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { dbankapp_backend } from 'declarations/dbankapp_backend';
+
+function App() {
+  const [greeting, setGreeting] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const name = event.target.elements.name.value;
+    dbankapp_backend.greet(name).then((greeting) => {
+      setGreeting(greeting);
+    });
+    return false;
+  }
+
+  return (
+    <main>
+      <img src="/logo2.svg" alt="DFINITY logo" />
+      <br />
+      <br />
+      <form action="#" onSubmit={handleSubmit}>
+        <label htmlFor="name">Enter your name: &nbsp;</label>
+        <input id="name" alt="Name" type="text" />
+        <button type="submit">Click Me!</button>
+      </form>
+      <section id="greeting">{greeting}</section>
+    </main>
+  );
+}
+
+export default App;
